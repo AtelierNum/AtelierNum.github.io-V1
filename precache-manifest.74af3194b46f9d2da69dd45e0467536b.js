3a6830f41baea6166915a50ee68e02f344ab6276
Simon-Renault <simon.renault.pro@gmail.com>
2019-09-25 17:43:24 +0100

    new version

---
+++ precache-manifest.74af3194b46f9d2da69dd45e0467536b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/App/dist/robots.txt"
+  },
+  {
+    "revision": "c8c4af0cf6a8ecedd6e1",
+    "url": "/App/dist/js/chunk-vendors.1eaf3f97.js"
+  },
+  {
+    "revision": "238a2cc88d8af0e97d72",
+    "url": "/App/dist/js/app.71f340a2.js"
+  },
+  {
+    "revision": "e265967ecf4bfd0e21f8b5629f81f83c",
+    "url": "/App/dist/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/App/dist/img/logo.82b9c7a5.png"
+  }
+];
