5260a872c28e257b10972b8a2488fd35c7c2c50f
Bérenger <berenger.recoules@gmail.com>
2020-05-28 10:19:21 +0200

    first responsive test

---
+++ App/dist/precache-manifest.17a1ed5e3a2b4651c59808a8aaefb36a.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = [
+  {
+    "revision": "c53bb519601216fca987",
+    "url": "css/app.ebfddddd.css"
+  },
+  {
+    "revision": "c53bb519601216fca987",
+    "url": "js/app.cf97561f.js"
+  },
+  {
+    "revision": "7b2be3afd9bbcea67bc5",
+    "url": "css/chunk-1eae2195.02085686.css"
+  },
+  {
+    "revision": "7b2be3afd9bbcea67bc5",
+    "url": "js/chunk-1eae2195.a2d0f1e7.js"
+  },
+  {
+    "revision": "1d1bc433000f219dcfb0",
+    "url": "css/chunk-25629552.71805d27.css"
+  },
+  {
+    "revision": "1d1bc433000f219dcfb0",
+    "url": "js/chunk-25629552.09c9197c.js"
+  },
+  {
+    "revision": "4bba2ad1e4c32956626b",
+    "url": "css/chunk-50617f7e.09a9adb0.css"
+  },
+  {
+    "revision": "4bba2ad1e4c32956626b",
+    "url": "js/chunk-50617f7e.afe810df.js"
+  },
+  {
+    "revision": "f4f4d57acafff2d1feb2",
+    "url": "css/chunk-547269b4.a144bc8b.css"
+  },
+  {
+    "revision": "f4f4d57acafff2d1feb2",
+    "url": "js/chunk-547269b4.8320a62c.js"
+  },
+  {
+    "revision": "61034e3f743b8905a141",
+    "url": "css/chunk-6a241350.096c0bbe.css"
+  },
+  {
+    "revision": "61034e3f743b8905a141",
+    "url": "js/chunk-6a241350.d9dd1873.js"
+  },
+  {
+    "revision": "3841bf8f4fc86d641cce",
+    "url": "css/chunk-6f266032.fea83b1d.css"
+  },
+  {
+    "revision": "3841bf8f4fc86d641cce",
+    "url": "js/chunk-6f266032.b9c650f9.js"
+  },
+  {
+    "revision": "2a9e5d9f22e5360bcb07",
+    "url": "css/chunk-917b9ca8.a4d9c713.css"
+  },
+  {
+    "revision": "2a9e5d9f22e5360bcb07",
+    "url": "js/chunk-917b9ca8.fb82baa8.js"
+  },
+  {
+    "revision": "4a20423feec453993a3b",
+    "url": "js/chunk-vendors.0627a868.js"
+  },
+  {
+    "revision": "1932da3071bff29228ccd96223cd865b",
+    "url": "img/roi_loth_gif.1932da30.gif"
+  },
+  {
+    "revision": "6efbd1502269d1ea42c00f651a45dbb9",
+    "url": "img/burgerLineNum.6efbd150.svg"
+  },
+  {
+    "revision": "3277cee66f2ae27c7d5c99cc2c8387c4",
+    "url": "img/footer_front_mask.3277cee6.svg"
+  },
+  {
+    "revision": "4df16443e4c08c5dc2d8fd844d0bab45",
+    "url": "img/footer_back_mask.4df16443.svg"
+  },
+  {
+    "revision": "3d157415ea0c23e91ee6efe5200f3fc1",
+    "url": "img/top_content_mask.3d157415.svg"
+  },
+  {
+    "revision": "c7650c55df622ba608fded77b81b4d0e",
+    "url": "img/mask_1.c7650c55.svg"
+  },
+  {
+    "revision": "647f8b7373430ceb069732d6fa3892aa",
+    "url": "img/logo_edna_white.647f8b73.png"
+  },
+  {
+    "revision": "04f1cd4b84b5014978c7b5b0a7ce3cb0",
+    "url": "index.html"
+  },
+  {
+    "revision": "324684a039c760e7825440c97130e41d",
+    "url": "data/globalList.json"
+  },
+  {
+    "revision": "647f8b7373430ceb069732d6fa3892aa",
+    "url": "img/logo_edna_white.png"
+  },
+  {
+    "revision": "1932da3071bff29228ccd96223cd865b",
+    "url": "img/roi_loth_gif.gif"
+  },
+  {
+    "revision": "02b1bb32a2cb2f4ba1db08b8a702bab7",
+    "url": "js/sketch.js"
+  },
+  {
+    "revision": "32094c4b41910839d87d85a1424da59a",
+    "url": "data/getContent.js"
+  },
+  {
+    "revision": "fb9745c0625c1f2b156941e20fa62c2e",
+    "url": "data/lists/projectsList.json"
+  },
+  {
+    "revision": "c6577c1b42dc0a1730c3fe6dcd3724a3",
+    "url": "data/lists/coursesList.json"
+  },
+  {
+    "revision": "555b9bf1e353f873ef36bc53fc6e7844",
+    "url": "data/lists/ressourcesList.json"
+  },
+  {
+    "revision": "ba0e7f69e1b6943a0e316e1eac212b91",
+    "url": "data/lists/tagsList.json"
+  },
+  {
+    "revision": "379c45b4d621033ff2f8537f4ef6a916",
+    "url": "icons/logo/logo@1x.png"
+  },
+  {
+    "revision": "1c8a54f25d101bdc607cec7228247a9a",
+    "url": "icons/socials/discord_logo.svg"
+  },
+  {
+    "revision": "a574348589bf998ac3e3767bdabbd80c",
+    "url": "icons/logo/lgo@2x.png"
+  },
+  {
+    "revision": "5b5846090d932223c9d39ec4d32133c9",
+    "url": "icons/socials/instagram.svg"
+  },
+  {
+    "revision": "084e6118b7302c403739be96c6c194ac",
+    "url": "icons/socials/twitter_logo.svg"
+  },
+  {
+    "revision": "50ca41cbbff6efd82f8638d1495c1e81",
+    "url": "icons/triangle_right.svg"
+  },
+  {
+    "revision": "532b58c73e9345e8466ac2766b8e9093",
+    "url": "img/logo_ateliernum.jpg"
+  },
+  {
+    "revision": "43e8d9795392ee7d5c730ae6941cff89",
+    "url": "img/logo_edna.png"
+  },
+  {
+    "revision": "df84ac63d2e2aa9a1194acdd10a1aeac",
+    "url": "js/sketchs/sketch.js"
+  },
+  {
+    "revision": "bc9c71d4d774beb1959eb38429889342",
+    "url": "js/sketchs/index.html"
+  },
+  {
+    "revision": "02531b99e14fafd9b3e7afcc4c98bbca",
+    "url": "img/logo_ateliernum.png"
+  },
+  {
+    "revision": "4df7f120f2d9952d54911aaf89042cb6",
+    "url": "icons/socials/github_logo.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "b4653a5fac810b436dd985c5569116fa",
+    "url": "js/p5.min.js"
+  }
+];
