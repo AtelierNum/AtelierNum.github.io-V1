b3ca4aa52c1123cfa98e2f39d21e5ab27287a105
Bérenger <berenger.recoules@gmail.com>
2020-05-28 15:54:41 +0200

    update build script to new folder structure

--- build.sh
+++ build.sh
@@ -8,19 +8,19 @@ echo 'Starting building ...'
 #cd ..
 #cd ./App && yarn run build
 #cd ..
-cd ./App/src/assets && node getContent.js
+cd ./App/public/data && node getContent.js
 cd ../../..
 cd ./App && npm run build
 cd ..
 
 #move everything to the right place
 git add .
-git commit -m $1
+git commit -m "new build"
 git push origin `git subtree split --prefix App/dist src`:master --force  
 
-#d o some cleanup
+#do some cleanup
 
-# deploy on github
+#deploy on github
 
 
 cd ..
