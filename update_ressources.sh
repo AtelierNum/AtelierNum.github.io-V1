027848f219a6fe3235571a4eb6d7e2516c368127
Bérenger <berenger.recoules@gmail.com>
2020-06-12 12:51:08 +0200

    new script to update ressources without new build

---
+++ update_ressources.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo 'Starting building ...'
+
+
+# build everything
+#cd ./Api && yarn run build
+#cd ..
+#cd ./App && yarn run build
+#cd ..
+cd ./App/public/data && node getContent.js
+cd ../../..
+
+
+#move everything to the right place
+git add .
+git commit -m "new ressource"
+git push origin `git subtree split --prefix App/dist src`:master --force  
+
+#do some cleanup
+
+#deploy on github
+
+
+cd ..
+
+
+
+
+echo 'done'
