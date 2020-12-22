32e5f9c41ef12f0c7e8a1f10b93d909902565042
Bérenger <berenger.recoules@gmail.com>
2020-12-22 11:26:23 +0100

    new build

--- App/dist/service-worker.js
+++ App/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.58388277dde6d225e9dcefe4f2803c35.js"
+  "precache-manifest.42183310895a70b5232531266a763172.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "ateliernum"});
