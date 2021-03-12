aefb430d1798f7d4989814ec29ddafc97bc27b02
Bérenger <berenger.recoules@gmail.com>
2021-03-12 09:11:24 +0100

    update build script

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 echo 'Starting building ...'
 
-
-
 cd ./public/data && node getContent.js
 cd ../..
 npm run build
@@ -12,8 +10,6 @@ npm run build
 git add .
 git commit -m "new build"
 git push origin `git subtree split --prefix dist src`:master --force  
-
-
 
 
 echo 'done'
