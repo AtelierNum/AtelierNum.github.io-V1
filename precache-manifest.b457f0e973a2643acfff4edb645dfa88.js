c8a53db90c36bba8f035ac793146d3e19c872c2c
b2renger <berenger.recoules@gmail.com>
2022-05-25 09:22:29 +0200

    new build

--- precache-manifest.b457f0e973a2643acfff4edb645dfa88.js
+++ precache-manifest.b457f0e973a2643acfff4edb645dfa88.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "c22ba6b27653431a4f5f",
+    "revision": "5ccb6f00e3dbdb4fbb62",
     "url": "css/app.5d23a68e.css"
   },
   {
-    "revision": "c22ba6b27653431a4f5f",
-    "url": "js/app.187226f0.js"
+    "revision": "5ccb6f00e3dbdb4fbb62",
+    "url": "js/app.f1f25fe5.js"
   },
   {
     "revision": "f1a4216c5b04dc882721",
@@ -88,12 +88,12 @@ self.__precacheManifest = [
     "url": "img/burgerLineNum.d7c86417.svg"
   },
   {
-    "revision": "3d157415ea0c23e91ee6efe5200f3fc1",
-    "url": "img/top_content_mask.3d157415.svg"
-  },
-  {
     "revision": "c7650c55df622ba608fded77b81b4d0e",
     "url": "img/mask_1.c7650c55.svg"
+  },
+  {
+    "revision": "3d157415ea0c23e91ee6efe5200f3fc1",
+    "url": "img/top_content_mask.3d157415.svg"
   },
   {
     "revision": "550597b56ab52739c0ac75d622aacc84",
@@ -112,11 +112,11 @@ self.__precacheManifest = [
     "url": "img/footer_back_mask.03d05ee5.svg"
   },
   {
-    "revision": "b61cc53470702d789dbd8b77b9a68fe4",
+    "revision": "2f697c9eadf53101532d636de8b50654",
     "url": "index.html"
   },
   {
-    "revision": "83ae116b0c60867fdcc2c5efbcf9515f",
+    "revision": "40c2160b9fb59d12c48db883e74392fa",
     "url": "data/globalList.json"
   },
   {
@@ -132,11 +132,7 @@ self.__precacheManifest = [
     "url": "js/sketch.js"
   },
   {
-    "revision": "b52a780013ad3a108e9c99f2882a36dc",
-    "url": "data/getContent.js"
-  },
-  {
-    "revision": "b004f7bf33f4b22926bc3165a6fd1903",
+    "revision": "7898e1cbc81acc2c035dd73ab07dfb24",
     "url": "data/lists/coursesList.json"
   },
   {
@@ -144,12 +140,12 @@ self.__precacheManifest = [
     "url": "data/lists/projectsList.json"
   },
   {
-    "revision": "5223a0e01602b0f06434c2fd21eb67d2",
+    "revision": "328d022abac9521caaaeb142cf744674",
     "url": "data/lists/ressourcesList.json"
   },
   {
-    "revision": "9345543ea29479efb200af9c3fa032e8",
-    "url": "data/lists/tagsList.json"
+    "revision": "a574348589bf998ac3e3767bdabbd80c",
+    "url": "icons/logo/lgo@2x.png"
   },
   {
     "revision": "1c8a54f25d101bdc607cec7228247a9a",
@@ -160,12 +156,16 @@ self.__precacheManifest = [
     "url": "icons/logo/logo@1x.png"
   },
   {
-    "revision": "a574348589bf998ac3e3767bdabbd80c",
-    "url": "icons/logo/lgo@2x.png"
+    "revision": "9345543ea29479efb200af9c3fa032e8",
+    "url": "data/lists/tagsList.json"
   },
   {
     "revision": "b30bac917a6beb84273fc550fd334f65",
     "url": "icons/socials/github_logo.svg"
+  },
+  {
+    "revision": "b52a780013ad3a108e9c99f2882a36dc",
+    "url": "data/getContent.js"
   },
   {
     "revision": "5b5846090d932223c9d39ec4d32133c9",
@@ -184,8 +184,16 @@ self.__precacheManifest = [
     "url": "img/logo_ateliernum.jpg"
   },
   {
+    "revision": "fbb0124726a23affe72bcb31ca59191a",
+    "url": "js/sketchs/index.html"
+  },
+  {
     "revision": "43e8d9795392ee7d5c730ae6941cff89",
     "url": "img/logo_edna.png"
+  },
+  {
+    "revision": "346701777e6e6f40a4f4e675882228bb",
+    "url": "js/sketchs/sketch.js"
   },
   {
     "revision": "735ab4f94fbcd57074377afca324c813",
@@ -194,14 +202,6 @@ self.__precacheManifest = [
   {
     "revision": "02531b99e14fafd9b3e7afcc4c98bbca",
     "url": "img/logo_ateliernum.png"
-  },
-  {
-    "revision": "346701777e6e6f40a4f4e675882228bb",
-    "url": "js/sketchs/sketch.js"
-  },
-  {
-    "revision": "fbb0124726a23affe72bcb31ca59191a",
-    "url": "js/sketchs/index.html"
   },
   {
     "revision": "5f9031303338096c833da870e0d77053",
